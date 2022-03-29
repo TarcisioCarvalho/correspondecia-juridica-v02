472f0e5aad1ddac091f9b1050d4c549631bdcf79
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-03-29 13:44:06 -0300

    Arrumando foto e menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,9 @@ btnMobile.addEventListener('touchstart',toggleMenu);
 function toggleMenu(event){
     if(event.type === 'touchstart') event.preventDefault();
     const menu = document.querySelector('[data-js="menu"]');
+    const nav = document.querySelector('[data-js="nav"]');
     menu.classList.toggle('active');
+    nav.classList.toggle('active');
 }
 
 
