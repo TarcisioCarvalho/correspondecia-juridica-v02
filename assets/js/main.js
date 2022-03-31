46a6af17729ea22e9322b7b05835b9138a1bbbed
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-03-31 20:13:22 -0300

    Alinhamento a esquerda, menu linkando a pagina

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,16 @@
 const btnMobile = document.querySelector('[data-js="button"]');
 btnMobile.addEventListener('click',toggleMenu);
 btnMobile.addEventListener('touchstart',toggleMenu);
+const linkSobre = document.querySelector('[data-js="sobre"]');
+const linkCorrespondencia = document.querySelector('[data-js="correspondecia-juridica"]');
+const areasAtuacao = document.querySelector('[data-js="areas-atuacao"]');
+const consultoriaLgpd = document.querySelector('[data-js="consultoria-lgpd"]');
+const contato = document.querySelector('[data-js="contato"]');
+linkSobre.addEventListener('click',toggleMenu);
+linkCorrespondencia.addEventListener('click',toggleMenu);
+areasAtuacao.addEventListener('click',toggleMenu);
+consultoriaLgpd.addEventListener('click',toggleMenu);
+contato.addEventListener('click',toggleMenu);
 
 function toggleMenu(event){
     if(event.type === 'touchstart') event.preventDefault();
